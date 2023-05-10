ae7dcafdb60d8d1a7bbc1b0a2506904e065b26c7
Yae1Ram0s <parksoja96@gmail.com>
2023-05-10 02:18:34 -0600

    interfaces graficas en Java

---
+++ src/Interfaces_Graficas/UI/eventos/EventosRaton.java
@@ -0,0 +1,32 @@
+package Interfaces_Graficas.UI.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventosRaton implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
